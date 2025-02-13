1e5794401930f60dd1873fb634ffad91653644a7
Josiel <cjosiel2@gmail.com>
2025-02-12 21:34:39 -0300

    Add project files.

---
+++ PrimeiraAplicacao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PrimeiraAplicacao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
