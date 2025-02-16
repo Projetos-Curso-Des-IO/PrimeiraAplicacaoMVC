22306b606d3b92531ffc98f23310867e561215e3
Josiel <cjosiel2@gmail.com>
2025-02-16 17:04:17 -0300

    SalvandoEditandoeRemovendo do banco

---
+++ PrimeiraAplicacao/Controllers/TesteEFController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraAplicacao.Data;
+using PrimeiraAplicacao.Models;
+
+namespace PrimeiraAplicacao.Controllers
+{
+	public class TesteEFController : Controller
+	{
+		public AppDbContext Db { get; set; }
+
+		public TesteEFController(AppDbContext db)
+		{
+			Db = db;
+		}
+
+		[Route("criarAluno")]
+		public IActionResult Index()
+		{
+
+			var aluno = new Aluno()
+			{
+				Nome = "Josiel",
+				Email = "josi2@gmail.com",
+				DataNascimento = new DateTime(2011, 02, 12),
+				Avaliacao = 6,
+				Ativo = true
+			};
+
+			//Db.Alunos.Add(aluno);
+			//Db.SaveChanges();
+
+			var alunoResultado = Db.Alunos.Where(a => a.Nome.Contains("Josiel")).FirstOrDefault();
+			alunoResultado.Nome = "Josiel costa martins";
+			alunoResultado.Avaliacao = 9;
+
+			Db.Alunos.Remove(alunoResultado);
+			//Db.Alunos.Update(alunoResultado);
+			Db.SaveChanges();
+		
+
+			return Content("");
+		}
+	}
+}
---
+++ PrimeiraAplicacao/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeiraAplicacao.Models;
+
+namespace PrimeiraAplicacao.Data
+{
+	public class AppDbContext : DbContext
+	{
+		// Construtor que recebe as opções de configuração
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+		public DbSet<Aluno> Alunos { get; set; }
+	}
+}
