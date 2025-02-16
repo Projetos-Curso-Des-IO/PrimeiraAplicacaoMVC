54d2534ccc8b4c792d1f5a3bf8aff1cb4fcc0b2c
Josiel <cjosiel2@gmail.com>
2025-02-16 13:55:30 -0300

    Concluido Views

---
+++ PrimeiraAplicacao/ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraAplicacao.Models;
+
+namespace PrimeiraAplicacao.ViewComponents
+{
+	public class SaudacaoAlunoViewComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var aluno = new Aluno() { Nome = "Joao" };
+			return View(aluno);
+		}
+
+
+	}
+}
